e4c578edb327a4a46c7bc099e9e8d7013f7c559e
w67254Hubert <w67254@student.wsiz.edu.pl>
2024-10-20 18:32:02 +0200

    update zad4

--- lab01/zad4.py
+++ lab01/zad4.py
@@ -34,23 +34,22 @@ def knapsackAlg(itemlist,maxw):
     for item in sortedItems: #poruszając się po posortowanych żeczach sprwawdzamy czy zmieścimy kolejną rzecz w plecaku
         backW=backW+ item[1]
         if backW<=maxw:
-            print(backW)
             profit=profit +item[2]
             itemsinback.append(item)
         
     return itemsinback,profit
-maxW=15 #max udźwig plecaka kg
 
+maxW=15 #max udźwig plecaka kg
 numOfItems=10
 itemlist=DoItemList(numOfItems)#lista rzeczy
 
 items,profit=knapsackAlg(itemlist,maxW)
 print(f'przedmioty w plecaku: nazwa,waga,wartość {items}')#wiem ze nie ładnie pokazuje ale walić
-print(len(items))
-waga=0
-for i in items:
-     waga=waga+i[1]
-print(waga)
+# print(len(items))
+# waga=0
+# for i in items:
+#      waga=waga+i[1]
+# print(waga)
 print(f'zysk z szabrowania {profit}')
 
 # wybierz przedmioty tak by zmaksywalizować wartość (bieresz max nagrodę z 1 kg) w plecaku nie przekraczając jego pojemnośći
