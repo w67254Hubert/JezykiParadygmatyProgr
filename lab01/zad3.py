fb92acfbd1da90d05e9fb24a1aa8a3a4c55aa225
w67254Hubert <w67254@student.wsiz.edu.pl>
2024-10-20 15:25:17 +0200

    zad 3

--- lab01/zad3.py
+++ lab01/zad3.py
@@ -1,26 +1,80 @@
-import random 
-import string
+
+import random
 
 #zadanie,czas wykonania, nagroda
-tasks=[["zadanie A",1,100],["zadanie B",2,30],["zadanie C",3,100],["zadanie D",2,50]]
+#tasks=[["zadanie A",1,100],["zadanie B",2,30],["zadanie C",3,100],["zadanie D",2,50]]
+
+def DoTaskList(listlen):
+    taskList=[]
+
+    listLengh=listlen
+
+    for i in range(listLengh):
+        taskName = "zad "+str(i)
+
+        time=random.randint(1,60) #czas
+
+        price=random.randint(1,100) #nagroda za wykonanie
+
+        task=[taskName,price,time]
+
+        taskList.append(task)
+    return taskList
+
+def rewardToTime(task):
+    return task[1] / task[2] #dzielenie czasu przez nagrodę wynik
+
+def sortTaskList(taskList):
+    # Sortuj według stosunku nagroda/czas, w kolejności malejącej
+    sorted_tasks = sorted(taskList, key=rewardToTime, reverse=True)
+    return sorted_tasks
+
+def WaitingTimeForTask(taskList): #czas po którym zadanie zostanie rozpoczęte
+    waitingTime = 0
+    timeslist=[]
+    for task in taskList:
+        name = task[0]
+        Time = task[2]
+        nametime=[name,waitingTime]
+        timeslist.append(nametime)
+        waitingTime +=Time
+
+    return timeslist
+
+
+def totalWaitingTime(taskList):
+    waitingTime = 0
+    for task in taskList:
+        Time = task[2]
+        waitingTime +=Time
+
+    return waitingTime
 
 
 
-taskList=[]
+listLen=20 #długość listy zadań
 
-listLengh=random.randint(1,10)
+taskList=DoTaskList(listLen)
 
-for i in range(listLengh):
-    
-    taskName = random.choice(string.ascii_uppercase)
+print('lista zadań:')
+print(taskList)
 
-    price=random.randint(1,100) #nagroda za wykonanie
+SortedTaskList=sortTaskList(taskList)
+print('kolejność wykonania przedstawionej listy zadań zadań:')
+# for task in SortedTaskList:
+#     print(task)
+print(SortedTaskList)
 
-    time=random.randint(1,60) #czas
+taskstartTime=WaitingTimeForTask(SortedTaskList) #czas po którym zadanie zostanie rozpoczęte
+print('czasy rozpoczęcia zadań:')
+print(taskstartTime)
+# for task in taskstartTime:
+#     print(task)
+totalTime=totalWaitingTime(taskList)
+print(f'Całkowity czas na wykonanie zadań wynosi {totalTime} min')
 
-    task=[taskName,price,time]
+#program tworzy liste zadań
+#następnie segreguje zadania na podstawie zalarzności czas / nagroda
+#liczy czas na wykonanie zadań z listy
 
-    taskList.append(task)
-
-    
-# print(taskList)
+#funkcja licząca czas w którym zostaną rozpoczęte zadania
